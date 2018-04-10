06166449dd51a0f2a846c9587308b57f3fc2a1ce
Maruf <kamruzzamanmaruf@gmail.com>
2018-04-10 21:57:39 +0600

    Fixed typeing errors.

--- angular5/src/app/home/home.component.ts
+++ angular5/src/app/home/home.component.ts
@@ -4,26 +4,26 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
   animations: [
 
   trigger('goals', [
-    transition('* => *',[
+    transition('* => *', [
 
       query(':enter', style({ opacity: 0 }), {optional: true}),
 
       query(':enter', stagger('300ms', [
         animate('.6s ease-in', keyframes([
-          style({opacity: 0, transform: 'translateY(-75%)', offset: 0;}),
-          style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3; }),
-          style({ opacity: 1.0, transform: 'translateY(0)', offset: 1.0; }),
+          style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+          style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
+          style({ opacity: 1.0, transform: 'translateY(0)', offset: 1.0})
         ]))]), {optional: true})
         ,
       query(':leave', stagger('300ms', [
         animate('.6s ease-in', keyframes([
-          style({ opacity: 1, transform: 'translateY(0)', offset: 0; }),
-          style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3; }),
-          style({ opacity: 0, transform: 'translateY(-75%)', offset: 1.0; }),
+          style({ opacity: 1, transform: 'translateY(0)', offset: 0}),
+          style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
+          style({ opacity: 0, transform: 'translateY(-75%)', offset: 1.0}),
         ]))]), { optional: true })
     ])
   ])
